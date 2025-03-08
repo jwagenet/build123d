8e0eb02f683c755d9e0feab4c6ccd6ac0055ebbc
Jonathan Wagenet <jonathanwagenet@gmail.com>
2025-03-08 12:47:40 -0500

    DoubleTangentArc:  This implementation seems complete, but now only works for other of type arc. (oops)
    - should now work on all planes by fixing rotation_axis and arc_tangent
    - will fail due to minimizer hitting max size. I think the limit can be changed, but leaving for now. See comment above error for info
    - removed unused initial conditions, they seem unnecessary, but might help in the future if max_size increases
    - use .cross to find 'normal' vectors

--- src/build123d/objects_curve.py
+++ src/build123d/objects_curve.py
@@ -173,7 +173,7 @@ class CenterArc(BaseEdgeObject):
 
         super().__init__(arc, mode=mode)
 
-from ocp_vscode import show_object
+
 class DoubleTangentArc(BaseEdgeObject):
     """Line Object: Double Tangent Arc
 
@@ -184,15 +184,14 @@ class DoubleTangentArc(BaseEdgeObject):
 
     Args:
         pnt (VectorLike): starting point of tangent arc
-        tangent (VectorLike): tangent at starting point of tangent arc
+        tangent (VectorLike): direction at starting point of tangent arc
         other (Union[Curve, Edge, Wire]): reference line
-        keep (Keep, optional): selector for which arc to keep when two arcs are
-            possible. The arc generated with TOP or BOTTOM depends on the geometry
-            and isn't necessarily easy to predict. Defaults to Keep.TOP.
+        keep (Keep, optional): select which arc to keep Defaults to Keep.TOP.
         mode (Mode, optional): combination mode. Defaults to Mode.ADD.
 
     Raises:
-        RunTimeError: no double tangent arcs found
+        RuntimeError: Point is already tangent to other!
+        RuntimeError: No double tangent arcs found.
     """
 
     _applies_to = [BuildLine._tag]
@@ -209,15 +208,11 @@ class DoubleTangentArc(BaseEdgeObject):
         validate_inputs(context, self)
 
         arc_pt = WorkplaneList.localize(pnt)
-        arc_tangent = WorkplaneList.localize(tangent).normalized()
-        # why isnt other localized?
+        wp_tangent = WorkplaneList.localize(tangent).normalized()
 
-
-
-        # changed this to match other curve objects
-        # I dont really understand what its for!
         if context is None:
-            workplane = Edge.make_line(arc_pt, arc_pt + arc_tangent).common_plane(
+            # Making the plane validates pnt, tangent, and other are coplanar
+            workplane = Edge.make_line(arc_pt, arc_pt + wp_tangent).common_plane(
                 *other.edges()
             )
             if workplane is None:
@@ -227,47 +222,42 @@ class DoubleTangentArc(BaseEdgeObject):
                 WorkplaneList._get_context().workplanes[0]
             )
 
-        # show_object([arc_pt, arc_tangent, workplane, ])
+        arc_tangent = Vector(tangent).transform(
+            workplane.reverse_transform, is_direction=True
+        ).normalized()
+        rotation_axis = Axis(workplane.origin, workplane.z_dir)
 
         # Determine where arc_point is located relative to other
         # ref forms a bisecting line parallel to arc tangent with same distance from other
         # center as arc point in direction of arc tangent
-        normal = Vector(arc_tangent.Y, -arc_tangent.X)
+        normal = arc_tangent.cross(workplane.z_dir)
         ref_scale = (other.arc_center - arc_pt).dot(-arc_tangent)
         ref = ref_scale * arc_tangent + other.arc_center
         ref_to_point = (arc_pt - ref).dot(normal)
 
-        # if ref_to_point != 0:
-        #     show_object([Line(other.arc_center, ref), Line(ref, arc_pt)])
-
-        if ref_to_point == other.radius * 2:
-            RuntimeError("Point is already linear tangent to other.")
+        keep_sign = -1 if keep == Keep.TOP else 1
+        if keep_sign * ref_to_point == other.radius:
+            raise RuntimeError("Point is already tangent to other!")
 
         # Use magnitude and sign of ref to arc point along with keep to determine
         #   which "side" angle the arc center will be on
-        # the arc center is the same side if the point is further from ref than other radius
-        # minimize type determines near or far side arc
-        keep_bool = keep == Keep.TOP
+        # - the arc center is the same side if the point is further from ref than other radius
+        # - minimize type determines near or far side arc to minimize to
         side_sign = 1 if ref_to_point < 0 else -1
-        type_bool = abs(ref_to_point) < other.radius
-
         minimize_type = 1
-        if type_bool:
+        if abs(ref_to_point) < other.radius:
+            # point/tangent pointing inside other, both arcs near
             if keep == Keep.TOP:
-                angle = -90
-            else:
                 angle = 90
+            else:
+                angle = -90
         else:
-            angle = side_sign * 90
+            # point/tangent pointing outside other, one near arc one far
+            angle = side_sign * -90
             if keep == Keep.TOP:
                 minimize_type = side_sign * -minimize_type
             else:
                 minimize_type = side_sign * minimize_type
-
-        if False:
-            print(pnt, tangent, arc_pt, arc_tangent, arc_pt + arc_tangent, other.normal())
-            print("length", ref_to_point, "radius", other.radius, "sign", side_sign, "type", type_bool)
-            print("keep", "top" if keep_bool else "bottom", "angle", angle, "type", "near" if minimize_type ==1 else "far")
 
         # Protect against massive circles that are effectively straight lines
         max_size = 10 * other.bounding_box().add(arc_pt).diagonal
@@ -276,7 +266,6 @@ class DoubleTangentArc(BaseEdgeObject):
         def func(radius, perpendicular_bisector, minimize_type):
             center = arc_pt + perpendicular_bisector * radius[0]
             separation = other.distance_to(center)
-            # show_object(CenterArc(center, radius[0], 0, 360), position=(0,3,100), target=(0,3,0), up="Y")
 
             if minimize_type == 1:
                 # near side arc
@@ -287,14 +276,7 @@ class DoubleTangentArc(BaseEdgeObject):
 
             return target
 
-        # Initial condition to speed up search
-        if minimize_type == 1:
-            initialize = other.distance_to(arc_pt)
-        elif minimize_type == -1:
-            initialize = other.distance_to(arc_pt) + 2 * other.radius
-
         # Find arc center by minimizing func result
-        rotation_axis = Axis((0, 0, 0), -other.normal())
         perpendicular_bisector = arc_tangent.rotate(rotation_axis, angle)
         result = minimize(
             func,
@@ -309,16 +291,25 @@ class DoubleTangentArc(BaseEdgeObject):
 
         # dir needs to be flipped for far arc
         tangent_normal = (other.arc_center - arc_center).normalized()
-        tangent_dir = minimize_type * Vector(tangent_normal.Y, -tangent_normal.X)
+        tangent_dir = minimize_type * tangent_normal.cross(workplane.z_dir)
         tangent_point = arc_radius * tangent_normal + arc_center
 
         # Confirm tangent point is on other
         if abs(other.radius - (tangent_point - other.arc_center).length) > TOLERANCE:
-            print("arc", arc_radius, "max", max_size)
-            print((tangent_point - other.arc_center).length)
+            # If we find the point on other where the tangent is parallel to arc tangent
+            # 1. form a line 1 following that tangent
+            # 2. form a line 2 from arc point to that point
+            # as the distance between the line 1 and arc_point goes to 0 and
+            # the angle between line 2 and arc tangent approaches 0 or 180
+            # the minimize will fail at max_size
+            #
+            # distance = ref_to_point + minimize_type * (angle / 90) * other.radius
+            # angle = (other.arc_center - side_sign * normal * other.radius - arc_pt).get_angle(arc_tangent)
+            #
+            # This should be the only way this error arises
             raise RuntimeError("No double tangent arc found, no tangent point found")
 
-        # Confirm tangent point is cotangent with point on other
+        # Confirm new tangent point is colinear with point tangent on other
         other_dir = other.tangent_at(tangent_point)
         if tangent_dir.get_angle(other_dir) > TOLERANCE:
             raise RuntimeError("No double tangent arc found, found tangent out of tolerance")
